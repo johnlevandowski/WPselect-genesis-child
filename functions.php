14b47995495143a415fcc9ce3271ab949a433e4f
John Levandowski <johnlevandowski@John-Levandowskis-MacBook.local>
2012-07-18 16:15:57 -0600

    Move secondary nab, add version to stylesheet, custom post-info, custom credits, post excerpt length and more link, lower jpeg quality of media

--- functions.php
+++ functions.php
@@ -32,6 +32,56 @@ function wpselect_custom_header_style() {
 /** Add support for 3-column footer widgets */
 add_theme_support( 'genesis-footer-widgets', 3 );
 
+/** Move secondary nav menu above header */
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_before_header', 'genesis_do_subnav' );
+
+/** Add version number to stylesheet genesis/lib/structure/layout.php */
+remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
+add_action( 'genesis_meta', 'wpselect_load_stylesheet' );
+function wpselect_load_stylesheet() {
+	/** Show Child Info */
+	$child_info = wp_get_theme() ;
+	echo '<link rel="stylesheet" href="' . get_bloginfo( 'stylesheet_url' ) . '?ver=' . esc_attr( $child_info['Version'] ) . '" type="text/css" media="screen" />'."\n";
+}
+
+/** Customize the post info function genesis/lib/structure/post.php */
+add_filter( 'genesis_post_info', 'wpselect_post_info' );
+function wpselect_post_info($post_info) {
+if (!is_page()) {
+    $post_info = '[post_date] [post_edit] [post_comments]';
+    return $post_info;
+	}
+}
+
+/** Customize the credits genesis/lib/structure/footer.php */
+add_filter('genesis_footer_creds_text', 'wpselect_footer_creds_text');
+function wpselect_footer_creds_text($creds_text) {
+	$creds_text = 'Powered by <a href="http://www.studiopress.com/themes/genesis" title="Genesis Framework">Genesis</a>, <a href="http://wpselect.com/go/hostgator/" title="Hosting by HostGator">HostGator</a>, [footer_wordpress_link] and ';
+	$creds_text .= get_num_queries(). ' MySQL queries in ' . timer_stop($display = 0, $precision = 2) . ' seconds<br />';
+	$creds_text .= 'Copyright [footer_copyright] &middot; <a href="/privacy-policy/" title="Privacy Policy">Privacy Policy</a> &middot; <a href="/disclaimer/" title="Disclaimer">Disclaimer</a> &middot; <a href="/ftc-disclosure/" title="FTC Disclosure">FTC Disclosure</a> &middot; <a href="/image-attribution/" title="Image Attribution">Image Attribution</a>';
+	return $creds_text;
+}
+
+/** Modify the length of post excerpts 55 default */
+add_filter( 'excerpt_length', 'wpselect_excerpt_length' );
+function wpselect_excerpt_length($length) {
+    return 60;
+}
+
+/** Modify the more link of post excerpts */
+add_filter('excerpt_more', 'wpselect_excerpt_more');
+function wpselect_excerpt_more($more) {
+	global $post;
+	return ' ... <a href="' . get_permalink($post->ID) . '">[Read more...]</a>';
+}
+
+/** Lower jpeg quality for media */
+add_filter( 'jpeg_quality', 'wpselect_jpeg_quality' );
+function wpselect_jpeg_quality( $quality ) {
+	return (int)79;
+}
+
 /** Before Content Sidebar Wrap Widget */
 genesis_register_sidebar( array(
 	'id'			=> 'wpselect_before_content_sidebar_wrap',
